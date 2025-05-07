8d1c0889addb0db05172a43c44118d3b766be088
ESNEB-DEV <bentzas@gmail.com>
2025-05-07 23:48:50 +0700

    update LoginForm-1

---
+++ src/componant/LoginForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LoginForm() {
+    return (
+        <div>
+            <form>
+                <input style={{border:'1px solid black'}} type="email" id="email" required/>
+            </form>
+        </div>
+
+    )
+}
+
+export default LoginForm
