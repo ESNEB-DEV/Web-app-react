1c107526217139c1fdf36820b9849024b29f4f38
ESNEB-DEV <bentzas@gmail.com>
2025-05-08 15:08:57 +0700

    update nevbar-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './componants/Navbar'
+import Features from './componants/Features'
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <div><Navbar /></div>
   )
 }
 
